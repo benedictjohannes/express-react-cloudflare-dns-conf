5ce5aa9270795a24d05f80b07f87709b2fab1a98
Benedict Yappy <yappy.benedict@gmail.com>
2020-03-21 23:22:23 +0700

    static build directory

---
+++ static/precache-manifest.1dbb13da23ae173330860eb58c15410c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bd9947a58fb0ad90ceaccdfaba1f272",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b9955b651a6fd49cb1f",
+    "url": "/static/css/main.c2775213.chunk.css"
+  },
+  {
+    "revision": "f6e033c61a0a00be8bab",
+    "url": "/static/js/2.6e57ccc2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6e57ccc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b9955b651a6fd49cb1f",
+    "url": "/static/js/main.9b7fdf8c.chunk.js"
+  },
+  {
+    "revision": "dd267b4ba446f7110cd5",
+    "url": "/static/js/runtime-main.6998140d.js"
+  }
+]);
